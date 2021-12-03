6dc614f667ac87c2690543b1adca084857c069c0
F1xa <Dubineckiy@bk.ru>
2021-12-03 18:06:41 +0300

    feat: add UI elements

---
+++ calculator/main.js
@@ -0,0 +1,5 @@
+const buttonDeleteAll = document.querySelector('.delete-all');
+const buttonDelete = document.querySelector('.delete');
+const buttonOperators = document.querySelectorAll('.operator');
+const buttonNumbers = document.querySelectorAll('.number');
+const output = document.querySelector('.output');
