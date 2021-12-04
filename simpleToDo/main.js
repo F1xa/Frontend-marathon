ba223b7dac59874146c9176f9d6716540163e130
F1xa <Dubineckiy@bk.ru>
2021-12-05 02:52:19 +0300

    feat: add modules and function addTask

--- simpleToDo/main.js
+++ simpleToDo/main.js
@@ -1,95 +1,142 @@
-let counterId = 3;
-
-const list = [{
-    id: 1,
-    name: "create a post",
-    status: "TODO",
-    priority: "low",
-  },
-  {
-    id: 2,
-    name: "test",
-    status: "Done",
-    priority: "high",
-  },
-];
+import {btn, inputHight, section} from './view.js'
 
 
-function changeStatus(task, status) {
+function addTask() {
+  btn.addEventListener('click', ()=> {
+    const newBlock = document.createElement('div');
+    const label = document.createElement('label');
+    const output = document.createElement('span');
+    const btnDelete = document.createElement('button');
+    
+    newBlock.className = 'wrap-check-box section';
+    output.className = 'content';
+    btnDelete.className = 'button-del';
+  
+    output.textContent = inputHight.value;
+    
+    section.after(newBlock);
+    newBlock.append(label);
+  
+    label.insertAdjacentHTML("afterbegin", '<input type="checkbox" class="check-box" name="checkbox">');
+    label.insertAdjacentHTML("beforeend", '<span class="check-box-style"></span>');
+    label.append(output);
+    
+    newBlock.append(btnDelete);
 
-  return list.find(item => item.name === task ? item.status = status : false);
-
+  btnDelete.addEventListener('click', ()=> {
+    newBlock.remove();
+  })
+   
+  })
 }
 
-
-
-function changePriority(task, priority) {
-
-  return list.find(item => item.name === task ? item.priority = priority : false);
-}
+addTask();
 
 
 
-function addTask(task, status = "TODO", priority = "high") {
-  list.push({
-    id: counterId++,
-    name: task,
-    status,
-    priority,
-  });
-}
 
 
 
-function deleteTask(id) {
-
-  return list.find((item, index) => item.id === id ? list.splice(index, 1) : false);
-}
 
 
 
-function showBy(group) {
-
-  function sortedStatus(status) {
-    return list.filter(item => item.status === status).map(item => item.name).join(', ');
-  }
-
-  function sortedPriority(priority) {
-    return list.filter(item => item.priority === priority).map(item => item.name).join(', ');
-  }
-
-
-  if (group === 'status') {
-
-    console.log(`TODO: ${sortedStatus("TODO")}`);
-    console.log(`In progress: ${sortedStatus("In progress")}`);
-    console.log(`Done: ${sortedStatus("Done")}\n`);
-
-  } else if (group === 'priority') {
-
-    console.log(`high: ${sortedPriority('high')}`);
-    console.log(`low: ${sortedPriority('low')}\n`);
-
-  }
-}
 
 
 
-addTask("go to the gym");
-addTask("have lunch");
-addTask("take a walk");
-addTask("read a book");
-addTask("clean the room")
 
-changeStatus("create a post", "In progress");
-changeStatus("take a walk", "Done");
+// let counterId = 3;
 
-changePriority("create a post", "low");
+// const list = [{
+//     id: 1,
+//     name: "create a post",
+//     status: "TODO",
+//     priority: "low",
+//   },
+//   {
+//     id: 2,
+//     name: "test",
+//     status: "Done",
+//     priority: "high",
+//   },
+// ];
 
-deleteTask(3);
-deleteTask(4);
 
-showBy('status');
-showBy('priority');
+// function changeStatus(task, status) {
 
-console.log(list)
+//   return list.find(item => item.name === task ? item.status = status : false);
+
+// }
+
+
+
+// function changePriority(task, priority) {
+
+//   return list.find(item => item.name === task ? item.priority = priority : false);
+// }
+
+
+
+// function addTask(task, status = "TODO", priority = "high") {
+//   list.push({
+//     id: counterId++,
+//     name: task,
+//     status,
+//     priority,
+//   });
+// }
+
+
+
+// function deleteTask(id) {
+
+//   return list.find((item, index) => item.id === id ? list.splice(index, 1) : false);
+// }
+
+
+
+// function showBy(group) {
+
+//   function sortedStatus(status) {
+//     return list.filter(item => item.status === status).map(item => item.name).join(', ');
+//   }
+
+//   function sortedPriority(priority) {
+//     return list.filter(item => item.priority === priority).map(item => item.name).join(', ');
+//   }
+
+
+//   if (group === 'status') {
+
+//     console.log(`TODO: ${sortedStatus("TODO")}`);
+//     console.log(`In progress: ${sortedStatus("In progress")}`);
+//     console.log(`Done: ${sortedStatus("Done")}\n`);
+
+//   } else if (group === 'priority') {
+
+//     console.log(`high: ${sortedPriority('high')}`);
+//     console.log(`low: ${sortedPriority('low')}\n`);
+
+//   }
+// }
+
+
+
+// addTask("go to the gym");
+// addTask("have lunch");
+// addTask("take a walk");
+// addTask("read a book");
+// addTask("clean the room")
+
+// changeStatus("create a post", "In progress");
+// changeStatus("take a walk", "Done");
+
+// changePriority("create a post", "low");
+
+// deleteTask(3);
+// deleteTask(4);
+
+// showBy('status');
+// showBy('priority');
+
+// console.log(list)
+
---
+++ simpleToDo/view.js
@@ -0,0 +1,3 @@
+export const btn = document.querySelector('.button');
+export const inputHight = document.querySelector('.input-hight');
+export const section = document.querySelector('.section');
